4ba279e4c55c92d7ef3ee1d61665a8ec5997111c
Emin Saygı <emin_0001@hotmail.com>
2021-12-29 14:06:57 +0300

    js recent added

---
+++ assets/img/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+
+const App =() => { 
+    return (
+    <SafeAreaView>
+        <Text>Hello World</Text>
+    </SafeAreaView>)}
+
+
+
+export default App;
